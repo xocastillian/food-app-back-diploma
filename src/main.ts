fc5db2ed9c6f0797300d6bf6267be937818df9bd
timqa <xocastillian@gmail.com>
2025-02-20 21:07:04 +0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
+}
+bootstrap();
