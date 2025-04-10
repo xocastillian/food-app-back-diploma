1c1d654ac057d406e55dd1b21dc5aba8fcacaa4b
Timqa <xocastillian@gmail.com>
2025-04-10 21:48:02 +0500

    cors opened

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://192.168.0.17:3001'],
+    origin: true,
     credentials: true,
   });
 
