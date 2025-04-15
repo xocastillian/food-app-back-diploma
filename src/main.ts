8a16422b164d6894b45bb3ddd9b70fdeeede300b
Timqa <xocastillian@gmail.com>
2025-04-15 20:42:21 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://192.168.0.12:3001'],
+    origin: [
+      'http://localhost:3001',
+      'http://192.168.0.12:3001',
+      'https://food-upnis-front.onrender.com',
+    ],
     credentials: true,
   });
 
